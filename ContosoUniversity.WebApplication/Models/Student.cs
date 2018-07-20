f044f15302348b4d44c2d216761e7db39579a549
Leandro Silveira Prado <leandro.prado@outlook.com.br>
2018-07-20 10:12:24 -0300

    alteração das colunas com StringLength

--- ContosoUniversity.WebApplication/Models/Student.cs
+++ ContosoUniversity.WebApplication/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,17 @@ namespace ContosoUniversity.WebApplication.Models
     public class Student
     {
         public int ID { get; set; }
+
+        [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 characters.")]
         public string LastName { get; set; }
+
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
+
         public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
