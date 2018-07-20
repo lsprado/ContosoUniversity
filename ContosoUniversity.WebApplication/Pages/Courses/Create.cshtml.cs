3e96beed1d006021ed596bb3c002c36a41736609
Leandro Silveira Prado <leandro.prado@outlook.com.br>
2018-07-20 16:33:19 -0300

    add  courses pages

---
+++ ContosoUniversity.WebApplication/Pages/Courses/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ContosoUniversity.WebApplication.Data;
+using ContosoUniversity.WebApplication.Models;
+
+namespace ContosoUniversity.WebApplication.Pages.Courses
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ContosoUniversity.WebApplication.Data.SchoolContext _context;
+
+        public CreateModel(ContosoUniversity.WebApplication.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "DepartmentID");
+            return Page();
+        }
+
+        [BindProperty]
+        public Course Course { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Courses.Add(Course);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ContosoUniversity.WebApplication/Pages/Courses/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.WebApplication.Data;
+using ContosoUniversity.WebApplication.Models;
+
+namespace ContosoUniversity.WebApplication.Pages.Courses
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ContosoUniversity.WebApplication.Data.SchoolContext _context;
+
+        public IndexModel(ContosoUniversity.WebApplication.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Course> Course { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Course = await _context.Courses
+                .Include(c => c.Department).ToListAsync();
+        }
+    }
+}
