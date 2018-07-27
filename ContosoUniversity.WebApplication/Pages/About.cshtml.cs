60e313f15ba3f6fce4f2d544bef3bc39d1fe9caa
Leandro Silveira Prado <leandro.prado@outlook.com.br>
2018-07-27 12:51:50 -0300

    remove Enrollments pages

--- ContosoUniversity.WebApplication/Pages/About.cshtml.cs
+++ ContosoUniversity.WebApplication/Pages/About.cshtml.cs
@@ -11,27 +11,15 @@ namespace ContosoUniversity.WebApplication.Pages
 {
     public class AboutModel : PageModel
     {
-        private readonly SchoolContext _context;
-
-        public AboutModel(SchoolContext context)
+        public AboutModel()
         {
-            _context = context;
         }
 
         public IList<EnrollmentDateGroup> Student { get; set; }
 
         public async Task OnGetAsync()
         {
-            IQueryable<EnrollmentDateGroup> data =
-                from student in _context.Student
-                group student by student.EnrollmentDate into dateGroup
-                select new EnrollmentDateGroup()
-                {
-                    EnrollmentDate = dateGroup.Key,
-                    StudentCount = dateGroup.Count()
-                };
-
-            Student = await data.AsNoTracking().ToListAsync();
+            
         }
     }
 }
