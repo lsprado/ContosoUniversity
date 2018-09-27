5e350baeacb8f737af815f8c4e457c8ab8dda60d
Leandro Silveira Prado <leandro.prado@outlook.com.br>
2018-09-27 14:19:21 +0000

    Merged PR 56: PR - Add custom metrics - app insights
    add custom metrics - app insights
    
    Related work items: #1246

--- ContosoUniversity.WebApplication/Pages/Students/Index.cshtml.cs
+++ ContosoUniversity.WebApplication/Pages/Students/Index.cshtml.cs
@@ -3,18 +3,22 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Net.Http;
 using Newtonsoft.Json;
+using Microsoft.ApplicationInsights;
+using System.Collections.Generic;
 
 namespace ContosoUniversity.WebApplication.Pages.Students
 {
     public class IndexModel : PageModel
     {
         private readonly IHttpClientFactory client;
+        private TelemetryClient telemetry;
 
         public string CurrentFilter { get; set; }
         
         public IndexModel(IHttpClientFactory client)
         {
             this.client = client;
+            this.telemetry = new TelemetryClient();
         }
 
         public Models.APIViewModels.StudentResult Student { get;set; }
@@ -28,6 +32,15 @@ namespace ContosoUniversity.WebApplication.Pages.Students
             }
             else
             {
+                // Set up some properties and metrics:
+                var properties = new Dictionary<string, string>
+                    {{"action", "StudentSearch"}, {"filter", SearchString}};
+                //var metrics = new Dictionary<string, double>
+                //    {{"Score", currentGame.Score}, {"Opponents", currentGame.OpponentCount}};
+
+                // Send the event:
+                telemetry.TrackEvent("SearchStudent", properties);
+
                 var response = await client.CreateClient("client").GetStringAsync("api/Students/Search?name=" + SearchString);
                 Student = JsonConvert.DeserializeObject<Models.APIViewModels.StudentResult>(response);
             }
