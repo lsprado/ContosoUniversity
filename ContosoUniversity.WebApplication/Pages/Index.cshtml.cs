a1c460b2800ed0f4877cc8ef6177e4c29dee68e3
Leandro Silveira Prado <leandro.prado@outlook.com.br>
2018-10-29 10:28:37 -0300

    add variable Ambiente

--- ContosoUniversity.WebApplication/Pages/Index.cshtml.cs
+++ ContosoUniversity.WebApplication/Pages/Index.cshtml.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace ContosoUniversity.WebApplication.Pages
 {
     public class IndexModel : PageModel
     {
+        public IConfiguration Configuration { get; }
+
+
+        public IndexModel(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void OnGet()
         {
-
+            var section = Configuration.GetSection("Ambiente");
+            ViewData["Ambiente"] = section.Value.ToString();
         }
     }
 }
