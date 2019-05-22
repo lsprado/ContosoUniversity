107483a7dbb679a86f219fa807e35f7324c62cbb
Leandro Silveira Prado <leandro.prado@outlook.com.br>
2019-05-22 17:50:47 -0300

    add import xml

---
+++ Scripts/ImportPolicyToAPIM.ps1
@@ -0,0 +1,12 @@
+﻿Write-Host "Import XML Policy"
+
+$agentDirectory = $env:Agent_ReleaseDirectory
+Write-Host "Agent directory = " $agentDirectory
+
+$pathXml = $agentDirectory + "\PoliciesAPIM.xml"
+Write-Host "Path Swagger = " $pathSwagger
+
+$apimContext = New-AzApiManagementContext -ResourceGroupName "RG_ContosoUniversity" -ServiceName "ContosoUniversity"
+Set-AzApiManagementPolicy -Context $apimContext -ApiId "ff86ccbd70a1488ab7fea0ce255a420b" -PolicyFilePath $pathXml
+
+Write-Host "Import successful"
