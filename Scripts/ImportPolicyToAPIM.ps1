4eddd447db4c82ca02eb51c0249991a7312af836
Leandro Silveira Prado <leandro.prado@outlook.com.br>
2019-05-22 18:22:12 -0300

    alterado o path para o XML policy

--- Scripts/ImportPolicyToAPIM.ps1
+++ Scripts/ImportPolicyToAPIM.ps1
@@ -3,8 +3,11 @@
 $agentDirectory = $env:Agent_ReleaseDirectory
 Write-Host "Agent directory = " $agentDirectory
 
-$pathXml = $agentDirectory + "\PoliciesAPIM.xml"
-Write-Host "Path Swagger = " $pathSwagger
+$primaryArtifact = $env:RELEASE_PRIMARYARTIFACTSOURCEALIAS
+Write-Host "Primary Artifact" + $primaryArtifact
+
+$pathXml = $agentDirectory + "\" + $primaryArtifact + "\Scripts\PoliciesAPIM.xml"
+Write-Host "Path XML Policy = " $pathXml
 
 $ApiMgmtContext = New-AzApiManagementContext -ResourceGroupName "RG_ContosoUniversity" -ServiceName "ContosoUniversity"
 Set-AzApiManagementPolicy -Context $ApiMgmtContext -ApiId "ContosoUniversityAPI" -PolicyFilePath $pathXml
