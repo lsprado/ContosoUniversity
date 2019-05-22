12be94e3288e3e35f6e6d38138ef5f76437615ea
Leandro Silveira Prado <leandro.prado@outlook.com.br>
2019-05-22 16:36:59 -0300

    add ps

---
+++ Scripts/CreateJsonFromSwagger.ps1
@@ -0,0 +1,9 @@
+﻿Write-Host "Create swagger.json files"
+
+$agentDirectory = $env:Agent_ReleaseDirectory
+Write-Host $agentDirectory
+
+$pathSwagger = $agentDirectory + "\swagger.json"
+Write-Host $pathSwagger
+
+Invoke-WebRequest -Uri "https://contosouniversityexampleapi-dev.azurewebsites.net/swagger/v1/swagger.json" | Set-Content -Path $pathSwagger
