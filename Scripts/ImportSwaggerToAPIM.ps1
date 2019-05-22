1997089ec7962b2644c11235251a8dc6cdbeea26
Leandro Silveira Prado <leandro.prado@outlook.com.br>
2019-05-22 17:00:14 -0300

    add ps

---
+++ Scripts/ImportSwaggerToAPIM.ps1
@@ -0,0 +1,8 @@
+﻿$agentDirectory = $env:Agent_ReleaseDirectory
+Write-Host "Agent directory = " + $agentDirectory
+
+$pathSwagger = $agentDirectory + "\swagger.json"
+Write-Host "Path Swagger = " + $pathSwagger
+
+$ApiMgmtContext = New-AzApiManagementContext -ResourceGroupName "RG_ContosoUniversity" -ServiceName "ContosoUniversity"
+Import-AzApiManagementApi -Context $ApiMgmtContext -SpecificationFormat "Swagger" -SpecificationPath $pathSwagger -Path "apis"
