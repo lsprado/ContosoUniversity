4239f2100288cd87460df7c096a681bdae8674b5
Leandro Silveira Prado <leandro.prado@outlook.com.br>
2020-10-27 16:25:43 -0300

    adicionado o projeto de framework

---
+++ ContosoUniversity.Framework/Util.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ContosoUniversity.Framework
+{
+    public class Util
+    {
+        public static bool ValidarCpf(string cpf)
+        {
+            if (String.IsNullOrEmpty(cpf))
+                return false;
+            else
+                return true;
+        }
+    }
+}
