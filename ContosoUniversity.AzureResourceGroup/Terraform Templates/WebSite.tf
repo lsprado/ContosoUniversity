9a5e3798029725a48c26cc4e5bb096eff590737f
Leandro Silveira Prado <leandro.prado@outlook.com.br>
2021-03-16 17:20:24 -0300

    alterado o plano para S2

--- ContosoUniversity.AzureResourceGroup/Terraform Templates/WebSite.tf
+++ ContosoUniversity.AzureResourceGroup/Terraform Templates/WebSite.tf
@@ -28,7 +28,7 @@ resource "azurerm_app_service_plan" "WebAppDemo" {
     resource_group_name = azurerm_resource_group.WebAppDemo.name
     sku {
         tier = "Standard"
-        size = "S1"
+        size = "S2"
     }
 }
 
